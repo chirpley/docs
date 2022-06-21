e2cdab7008c80a8ef91d26ed232d4b94b7816d13
chirpley <87661640+chirpley@users.noreply.github.com>
2022-06-21 20:39:34 +0200

    update sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinx_search.extension',   
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
